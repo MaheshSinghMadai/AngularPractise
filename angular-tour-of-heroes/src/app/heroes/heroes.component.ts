bc01d300d26b10ac583828dc70d1e815fb254f80
Mahesh Singh Madai <maheshmadai977@gmail.com>
2023-09-06 09:07:36 +0545

    Form issue solved

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IHero } from './IHero';
+import { Heroes } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,8 +7,5 @@ import { IHero } from './IHero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: IHero = {
-    id:1,
-    name:'Mahesh',
-  }
+  heroes = Heroes;
 }
